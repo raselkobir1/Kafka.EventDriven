b5963702b4e9c2c2a0ed1f9a4f02c89c02d0875b
Rasel Kabir <md.rasel@technonext.com>
2024-10-28 00:20:11 +0600

    ++ docker-compose file added

--- Kafka.EventDriven/Kafka.EventDriven/Controllers/Models/OrderDetails.cs
+++ Kafka.EventDriven/Kafka.EventDriven/Controllers/Models/OrderDetails.cs
@@ -1,10 +1,16 @@
-﻿namespace Kafka.EventDriven.Controllers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kafka.EventDriven.Controllers.Models
 {
     public class OrderDetails
     {
+        [Required]
         public int OrderId { get; set; }
+        [Required]
         public string? ProductName { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public DateTime OrderDate { get; set; }
     }
 }
