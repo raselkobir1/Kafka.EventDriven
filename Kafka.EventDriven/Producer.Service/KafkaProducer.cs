bc538e89ffe416e2fd96e9e7c91eddd23f09827a
Rasel Kabir <md.rasel@technonext.com>
2024-10-28 11:06:10 +0600

    ++ kafka prducer exception handle and log error

--- Kafka.EventDriven/Producer.Service/KafkaProducer.cs
+++ Kafka.EventDriven/Producer.Service/KafkaProducer.cs
@@ -1,4 +1,5 @@
 ﻿using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Producer.Service
@@ -6,21 +7,35 @@ namespace Producer.Service
     public class KafkaProducer : IKafkaProducer
     {
         private readonly IProducer<string, string> kafkaProducer;
-        public KafkaProducer(string bootstrapServers, string clientId)
+        private readonly ILogger<KafkaProducer> _logger;
+        public KafkaProducer(string bootstrapServers, string clientId, ILogger<KafkaProducer> logger = null)
         {
             var producerConfig = new ProducerConfig
             {
                 BootstrapServers = bootstrapServers,
                 ClientId = clientId,   
             };
-
+            _logger = logger;
             kafkaProducer = new ProducerBuilder<string, string>(producerConfig).Build();
         }
         public async Task Producer<T>(string topic, string key, T message) 
         {
-            var serialized_message = JsonConvert.SerializeObject(message);
-            await kafkaProducer.ProduceAsync(topic, new Message<string, string> { Key = key, Value = serialized_message });
+            try
+            {
+                var serialized_message = JsonConvert.SerializeObject(message);
+                var kafkaResult = await kafkaProducer.ProduceAsync(topic, new Message<string, string> { Key = key, Value = serialized_message });
+                if (kafkaResult != null)
+                {
+                    //send notifcation
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error,ex.Message);
+                throw;
+            }
         }
+
         public void ProducerDispose()
         {
             kafkaProducer?.Dispose();
